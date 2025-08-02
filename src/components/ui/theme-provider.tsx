9fff6e5f698a2f352b9c4ade5414c97e71f97428
Pawandasila <pawandasila06@gmail.com>
2025-08-03 03:02:40 +0530

    theeme provider

--- src/components/ui/theme-provider.tsx
+++ src/components/ui/theme-provider.tsx
@@ -2,8 +2,16 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: any
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+  [key: string]: any
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
+}``
